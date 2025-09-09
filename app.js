7d896480af3173cce30f2dc03a7f2031e97d19e1
janetmei <179970314+janetmei@users.noreply.github.com>
2025-09-09 20:22:54 -0300

    preparacion del puerto desde variables de entorno

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("hello world localhost:3000");
+import express from 'express';
+
+import { PORT } from './config/env.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Bienvenido a la API de seguimiento de suscripciones');
+});
+
+app.listen(PORT, () => {
+    console.log(`La API de seguimiento de suscripciones se está ejecutando en http://localhost:${PORT}`);
+});
+
+export default app;
+
